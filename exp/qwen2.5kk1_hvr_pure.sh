dc4e8603c79608428f06a063303e6ace78d55a31
elysiameik-afk <elysiameik@gmail.com>
2025-08-23 12:09:03 +0800

    Update qwen2.5kk1_hvr_pure.sh

--- exp/qwen2.5kk1_hvr_pure.sh
+++ exp/qwen2.5kk1_hvr_pure.sh
@@ -22,11 +22,11 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \ 
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.rollout.n=8 \
-    actor_rollout_ref.rollout.logprobs=1 \
+    +actor_rollout_ref.rollout.logprobs=1 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=False \
     actor_rollout_ref.rollout.max_num_batched_tokens=16384 \
